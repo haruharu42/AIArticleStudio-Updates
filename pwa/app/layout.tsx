8e7da62c31ecdd8d77f952c310c5bf1df0931620
haruharu42 <haruki.gx7.24.7@gmail.com>
2026-09-14 22:44:14 +0900

    Load adaptive navigation layout override

--- pwa/app/layout.tsx
+++ pwa/app/layout.tsx
@@ -22,6 +22,7 @@ import "./phase28-free-trial.css";
 import "./phase29-admin-compact-entitlements.css";
 import "./phase30-security-operations.css";
 import "./phase31-help-nav.css";
+import "./phase31-nav-flex.css";
 
 export const metadata: Metadata = {
   title: "AI記事スタジオ PWA",
