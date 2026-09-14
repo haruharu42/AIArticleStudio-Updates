1db3b3ec882f05d7dff70dc6dc890fbb5d339869
haruharu42 <haruki.gx7.24.7@gmail.com>
2026-09-14 18:16:23 +0900

    Move SNS trial quota to explicit generation action

--- pwa/app/sns/page.tsx
+++ pwa/app/sns/page.tsx
@@ -1,6 +1,5 @@
-import { FreeTrialFeatureGate } from "@/components/free-trial-feature-gate";
 import { Phase14SnsPage } from "@/components/phase14-sns-page";
 
 export default function SnsPage() {
-  return <FreeTrialFeatureGate feature="sns_generate"><Phase14SnsPage /></FreeTrialFeatureGate>;
+  return <Phase14SnsPage />;
 }
