98dd8fd9de6675a101f78aff8b29ba85a00c840e
haruharu42 <haruki.gx7.24.7@gmail.com>
2026-09-14 18:16:15 +0900

    Move image trial quota to explicit generation action

--- pwa/app/images/page.tsx
+++ pwa/app/images/page.tsx
@@ -1,6 +1,5 @@
-import { FreeTrialFeatureGate } from "@/components/free-trial-feature-gate";
 import { Phase13ImagePromptPage } from "@/components/phase13-image-page";
 
 export default function ImagesPage() {
-  return <FreeTrialFeatureGate feature="image_generate"><Phase13ImagePromptPage /></FreeTrialFeatureGate>;
+  return <Phase13ImagePromptPage />;
 }
