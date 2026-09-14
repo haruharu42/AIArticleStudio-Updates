21f735050c7f815b6678768d23007fc645d63f32
haruharu42 <haruki.gx7.24.7@gmail.com>
2026-09-14 18:16:06 +0900

    Move article trial quota to explicit generation action

--- pwa/app/create/page.tsx
+++ pwa/app/create/page.tsx
@@ -1,6 +1,5 @@
 import { CreateAiSetup } from "@/components/create-ai-setup";
-import { FreeTrialFeatureGate } from "@/components/free-trial-feature-gate";
 
 export default function CreatePage() {
-  return <FreeTrialFeatureGate feature="article_generate"><CreateAiSetup /></FreeTrialFeatureGate>;
+  return <CreateAiSetup />;
 }
