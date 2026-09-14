d1323a73cda012f7390efb49edfe6040d9a76394
haruharu42 <haruki.gx7.24.7@gmail.com>
2026-09-14 11:20:39 +0900

    Gate article creation by free trial usage

--- pwa/app/create/page.tsx
+++ pwa/app/create/page.tsx
@@ -1,5 +1,6 @@
 import { CreateAiSetup } from "@/components/create-ai-setup";
+import { FreeTrialFeatureGate } from "@/components/free-trial-feature-gate";
 
 export default function CreatePage() {
-  return <CreateAiSetup />;
+  return <FreeTrialFeatureGate feature="article_generate"><CreateAiSetup /></FreeTrialFeatureGate>;
 }
