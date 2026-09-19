a2289ce6dc30c2f748e8df29b258ec2290a01a07
haruharu42 <haruki.gx7.24.7@gmail.com>
2026-09-19 20:07:38 +0900

    Load distinct mobile and desktop layouts

--- pwa/app/layout.tsx
+++ pwa/app/layout.tsx
@@ -27,6 +27,7 @@ import "./phase31-nav-flex.css";
 import "./phase32-sales-settings.css";
 import "./phase33-reference-ui.css";
 import "./phase34-article-presets.css";
+import "./phase35-device-layout.css";
 
 export const metadata: Metadata = {
   title: "AI記事スタジオ PWA",
