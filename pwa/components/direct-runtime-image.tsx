3b84c6d53cb009a88a46639567036048bb385b97
haruharu42 <haruki.gx7.24.7@gmail.com>
2026-09-26 06:48:15 +0900

    Require alt text on runtime images

--- pwa/components/direct-runtime-image.tsx
+++ pwa/components/direct-runtime-image.tsx
@@ -1,8 +1,10 @@
 import type { ImgHTMLAttributes } from "react";
 
-export type DirectRuntimeImageProps = ImgHTMLAttributes<HTMLImageElement>;
+export type DirectRuntimeImageProps = Omit<ImgHTMLAttributes<HTMLImageElement>, "alt"> & {
+  alt: string;
+};
 
-export function DirectRuntimeImage(props: DirectRuntimeImageProps) {
+export function DirectRuntimeImage({ alt, ...props }: DirectRuntimeImageProps) {
   // eslint-disable-next-line @next/next/no-img-element -- Blob/Object URLs and expiring signed URLs must render directly without an optimizer fetch.
-  return <img {...props} />;
+  return <img alt={alt} {...props} />;
 }
